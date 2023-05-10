b53a2c4960118a802c75e49ffe55f5c438ebd6a8
will <guillermototi11@gmail.com>
2023-05-10 19:29:00 -0400

    clase sabado 6 de mayo 2023

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import testController from './controllers/test.controller';
+import userController from './controllers/user.controller';
 //import UserController from './controllers/usuario.controller';
 const app = express();
 app.use(cors({
@@ -18,5 +19,10 @@ app.use((err: any, req: any, res: any, next: any) => {
     }
 });
 app.get('/test', testController.test)
+app.post('/test/:id', testController.test)
+app.get('/user/list', userController.Obtener)
+app.post('/user/crear', userController.Crear)
+app.put('/user/editar/:id', userController.Editar)
+app.delete('/user/eliminar/:id', userController.Eliminar)
 //app.get('/api/usuario/test',UserController.test);
 export default app;
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { IUser, User } from "../entities/User";
+import { MysqlDataSource } from "../configs/Db";
+
+const repository = MysqlDataSource.getRepository(User);
+class UserController {
+
+
+    public async Obtener(req: Request, res: Response) {
+        res.send("")
+    }
+    public async Crear(req: Request, res: Response) {
+        const users: IUser = req.body;
+        const oUser = new User(users)
+        await repository.save(oUser)
+        res.send("")
+    }
+    public async Editar(req: Request, res: Response) {
+        res.send("")
+    }
+    public async Eliminar(req: Request, res: Response) {
+        res.send("")
+    }
+
+}
+
+export default new UserController();
